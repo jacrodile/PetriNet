84b46bdb9edaa29c543d5693f20760b2bbc7d391
samako96 <zenom.1996@gmail.com>
2022-03-18 11:24:26 +0100

    modified package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "PetriNet",
+    products: [
+      .library(name: "PetriNet", targets: ["PetriNet"]),
+    ],
     dependencies: [],
     targets: [
         .executableTarget(name: "PetriNet", dependencies: []),
